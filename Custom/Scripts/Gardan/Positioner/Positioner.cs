9a7e39fb378ef665ac0a136d8a87de1a14d8556a
Mike Tremp <mike.tremp@loanboox.ch>
2023-06-23 18:01:08 +0200

    Setting coordinates to camera now works.

--- Custom/Scripts/Gardan/Positioner/Positioner.cs
+++ Custom/Scripts/Gardan/Positioner/Positioner.cs
@@ -1,13 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using SimpleJSON;
 
 public class Positioner : MVRScript
 {
-    private static string SAVE_PREFIX = "Positioner_";
-
     private Atom _containingAtom;
     private JSONStorableBool _isPositionerHost;
     protected List<string> MonitorCoordinatesStringList = new List<string>();
@@ -18,16 +17,27 @@ public class Positioner : MVRScript
     protected InputField CoordsTextInputFieldUI;
     protected UIDynamicTextField coordsTextTitle;
     protected List<UIDynamic> coordsComponentsUI = new List<UIDynamic>();
-    private bool isInit = false;
+
+    protected bool isInit = false;
+
+    private JSONStorableBool _spawnOnEnable;
 
     public override void Init()
     {
+        isInit = true;
+
         _containingAtom = containingAtom;
 
         // add button
-        UIDynamicButton addCoordsBtn = CreateButton("Add coords", true);
+        UIDynamicButton addCoordsBtn = CreateButton("Add camera coords", true);
         addCoordsBtn.button.onClick.AddListener(() => { OnAddNewCoords(); });
         globalControlsUIs.Add((UIDynamic)addCoordsBtn);
+
+        // test button
+        UIDynamicButton setCoordsBtn = CreateButton("Set camera to coords", true);
+        setCoordsBtn.button.onClick.AddListener(() => { SetCoords(); });
+        globalControlsUIs.Add((UIDynamic)setCoordsBtn);
+
 
         // Create UI elements
         CreateCoordsUIelements();
@@ -35,6 +45,10 @@ public class Positioner : MVRScript
         // MonitorPosition choices
         monitorPositionChoices = new List<string>();
         MonitorPositionSelector = new JSONStorableStringChooser("Monitor position ID", monitorPositionChoices, "", "Monitor position ID");
+        MonitorPositionSelector.isRestorable = true;
+        MonitorPositionSelector.isStorable = true;
+        MonitorPositionSelector.storeType = JSONStorableParam.StoreType.Full;
+        RegisterStringChooser(MonitorPositionSelector);
         MonitorPositionSelector.setCallbackFunction += (val) => { OnTogglecoordsId(val); };
         UIDynamicPopup DSelsp = CreateScrollablePopup(MonitorPositionSelector, true);
         DSelsp.labelWidth = 250f;
@@ -42,10 +56,83 @@ public class Positioner : MVRScript
 
         SuperController.singleton.BroadcastMessage("OnActionsProviderAvailable", this, SendMessageOptions.DontRequireReceiver);
 
+
+        StartCoroutine(InitDeferred());
+
+
         if (enabled)
+        {
             OnEnable();
+        }
+
+        isInit = false;
     }
 
+    // Got this from AcidBubbles code, kudos.
+    private IEnumerator InitDeferred()
+    {
+        yield return new WaitForEndOfFrame();
+        if (!enabled) yield break;
+        if (_spawnOnEnable.val)
+            SetCoords();
+        yield return 0;
+        if (!enabled) yield break;
+        if (_spawnOnEnable.val)
+            SetCoords();
+    }
+
+    // We need to override this function, to save a custom array in the savegame file
+    public override JSONClass GetJSON(bool includePhysical = true, bool includeAppearance = true, bool forceStore = false)
+    {
+        //SuperController.LogMessage($"GETJSON called");
+
+        JSONClass jsonObject = base.GetJSON(includePhysical, includeAppearance, forceStore);
+
+        // Store MonitorCoordinatesStringList
+        JSONArray monitorCoordsArray = new JSONArray();
+        foreach (string coord in MonitorCoordinatesStringList)
+        {
+            monitorCoordsArray.Add(coord);
+        }
+        jsonObject["monitorCoordinatesStringList"] = monitorCoordsArray;
+
+        return jsonObject;
+    }
+
+    // We also need to override this to restore our custom array from the savegame file
+    public override void LateRestoreFromJSON(JSONClass jc, bool restorePhysical = true, bool restoreAppearance = true, bool setMissingToDefault = true)
+    {
+        base.LateRestoreFromJSON(jc, restorePhysical, restoreAppearance, setMissingToDefault);
+
+        JSONArray coordsList = jc["monitorCoordinatesStringList"].AsArray;
+        MonitorCoordinatesStringList.Clear();
+
+        // Fill our string list with the saved coordinates
+        foreach (JSONNode node in coordsList)
+        {
+            string coords = node.Value;
+            MonitorCoordinatesStringList.Add(coords);
+        }
+
+        // SuperController.LogMessage($"AFTER Load stringlist count: " + MonitorCoordinatesStringList.Count);
+
+        // Fill the slider with the ID's, so we can select the coordinates from the string list
+        monitorPositionChoices.Clear();
+        for (int i = 0; i < MonitorCoordinatesStringList.Count; i++)
+        {
+            monitorPositionChoices.Add(i.ToString());
+        }
+
+        MonitorPositionSelector.valNoCallback = "";
+        MonitorPositionSelector.choices = null; // force UI sync
+        MonitorPositionSelector.choices = monitorPositionChoices;
+
+        // default to 0
+        MonitorPositionSelector.val = "0";
+        UpdateTextField("0");
+    }
+
+    // This happens when the button add coords is pressed
     protected void OnAddNewCoords()
     {
         string coordsId = MonitorCoordinatesStringList.Count.ToString();
@@ -55,7 +142,7 @@ public class Positioner : MVRScript
         // get camera position
         var centerCameraPosition = sc.centerCameraTarget.transform.position;
 
-        // get camera rotation?
+        // get camera rotation
         var monitorCenterCameraRotation = sc.MonitorCenterCamera.transform;
 
         // Here we add coordinates to our coordinates list
@@ -69,23 +156,82 @@ public class Positioner : MVRScript
         UpdateTextField(coordsId);
     }
 
+    // Read the coordinates from the UI text field and set the camera to that position
+    protected void SetCoords()
+    {
+        string[] coordsStringArray = CoordsTextInputFieldUI.text.Split('_');
+
+        if (coordsStringArray.Length == 6)
+        {
+            try
+            {
+                Vector3 newCenterCameraPosition = new Vector3(float.Parse(coordsStringArray[0]), float.Parse(coordsStringArray[1]), float.Parse(coordsStringArray[2]));
+                Vector3 newMonitorCenterCameraRotation = new Vector3(float.Parse(coordsStringArray[3]), float.Parse(coordsStringArray[4]), float.Parse(coordsStringArray[5]));
+
+                SetCoords(newCenterCameraPosition, newMonitorCenterCameraRotation);
+            }
+            catch (Exception e)
+            {
+                SuperController.LogError($"Could not parse coordinates from the text field.");
+                SuperController.LogError(e.Message);
+            }
+        }
+        else
+        {
+            SuperController.LogError($"Could not parse coordinates from the text field, need exactly 6 coordinates (position x,y,z and rotation x,y,z).");
+        }
+    }
+
+
+    // Here we read the coordinates from the UI text field and set the camera to that position
+    protected void SetCoords(Vector3 newCenterCameraPosition, Vector3 newMonitorCenterCameraRotation)
+    {
+        // SuperController.LogMessage($"SetCoords called");
+
+        var sc = SuperController.singleton;
+        
+        // This part is copied from the SpawnPoint script from AcidBubbles -> Kudos man!
+        // I would have NEVER figured out, how to calculate this stuff
+        sc.ResetNavigationRigPositionRotation();
+
+        var navigationRigTransform = sc.navigationRig.transform;
+
+        navigationRigTransform.eulerAngles = newMonitorCenterCameraRotation;
+
+        var targetPosition = newCenterCameraPosition;
+        navigationRigTransform.eulerAngles = new Vector3(0, newMonitorCenterCameraRotation.y, 0);
+
+        var centerCameraPosition = sc.centerCameraTarget.transform.position;
+        var teleportPosition = targetPosition + (navigationRigTransform.position - centerCameraPosition);
+
+        navigationRigTransform.position = new Vector3(teleportPosition.x, 0, teleportPosition.z);
+        sc.playerHeightAdjust += (targetPosition.y - centerCameraPosition.y);
+
+        var monitorCenterCameraTransform = sc.MonitorCenterCamera.transform;
+        monitorCenterCameraTransform.eulerAngles = newMonitorCenterCameraRotation;
+        monitorCenterCameraTransform.localEulerAngles = new Vector3(monitorCenterCameraTransform.localEulerAngles.x, 0, 0);
+
+        //SuperController.LogMessage($"SetCoords end");
+    }
+
     public void UpdateTextField(string coordsId)
     {
-        SuperController.LogMessage($"updating textfield");
+        // SuperController.LogMessage("updating textfield");
 
         int coordsIdInt = Int32.Parse(coordsId);
-        CoordsTextInputFieldUI.text = MonitorCoordinatesStringList[coordsIdInt];
-        CoordsTextUI.val = CoordsTextInputFieldUI.text;
+        if (coordsIdInt >= 0 && coordsIdInt < MonitorCoordinatesStringList.Count)
+        {
+            CoordsTextInputFieldUI.text = MonitorCoordinatesStringList[coordsIdInt];
+            CoordsTextUI.val = CoordsTextInputFieldUI.text;
+        }
+        else
+        {
+            SuperController.LogError("Invalid coordsId: " + coordsId);
+        }
     }
 
     protected void RefreshSelectors(string coordsId)
     {
-        /*
-        SuperController.LogMessage($"refreshing selector");
-        SuperController.LogMessage($"MonitorPositionSelector.val1: " + MonitorPositionSelector.val);
-        SuperController.LogMessage($"MonitorPositionSelector.valNoCallback1: " + MonitorPositionSelector.valNoCallback);
-        */
-
         // we need to add our monitor id to the choice selector
         monitorPositionChoices.Add(coordsId);
 
@@ -93,19 +239,10 @@ public class Positioner : MVRScript
         MonitorPositionSelector.choices = null; // force UI sync
         MonitorPositionSelector.choices = monitorPositionChoices;
         MonitorPositionSelector.val = coordsId;
-
-        /*
-        SuperController.LogMessage($"monitor position choices: " + MonitorPositionSelector.choices.Count);
-        SuperController.LogMessage($"first monitor position choice: " + MonitorPositionSelector.choices[0]);
-        SuperController.LogMessage($"last monitor position choice: " + MonitorPositionSelector.choices[MonitorPositionSelector.choices.Count - 1]);
-        SuperController.LogMessage($"MonitorPositionSelector.val2: " + MonitorPositionSelector.val);
-        */
     }
 
     protected void OnTogglecoordsId(string coordsId)
     {
-        //SuperController.LogMessage($"Coords id was toggled, isinit? " + isInit);
-
         // here we get the selected ID and want to update the text field
         UpdateTextField(coordsId);
     }
@@ -123,7 +260,7 @@ public class Positioner : MVRScript
         coordsComponentsUI.Add((UIDynamic)coordsTextTitle);
 
         // ******* COORDS TEXTFIELD TEXT ***********
-        string newDefaultText = "Type some text...";
+        string newDefaultText = "Treat this as a read-only field, don't type in it.";
         CoordsTextUI = new JSONStorableString("CoordsTextUI", "_default_");
         tmpTextfield = CreateTextField(CoordsTextUI);
         SetupTextField(tmpTextfield, 550f, false, false);
@@ -203,7 +340,6 @@ public class Positioner : MVRScript
         SuperController.singleton.BroadcastMessage("OnActionsProviderDestroyed", this, SendMessageOptions.DontRequireReceiver);
     }
 
-
     public class MonitorCoordinates
     {
         protected JSONClass coordsData { get; set; }
@@ -217,20 +353,12 @@ public class Positioner : MVRScript
             MonitorRotation = monitorRotation;
         }
 
+        // This is the string representation of the monitor coordinates, that we can parse again later
+        // I'm sure there's a fancy way of doing this in JSON
         public string MonitorCoordsToString()
         {
             string coordsAsString = MonitorPosition.x + "_" + MonitorPosition.y + "_" + MonitorPosition.z + "_" + MonitorRotation.eulerAngles.x + "_" + MonitorRotation.eulerAngles.y + "_" + MonitorRotation.eulerAngles.z;
             return coordsAsString;
-        }
-
-        public void Delete()
-        {
-            // Clearing data from the loaded data of the scene (to prevent re-restore after deleting)
-            // This only happens when you load a scene and have data loaded from the json
-            if (coordsData != null)
-            {
-                coordsData.Remove(SAVE_PREFIX + "_" + MonitorCoordsID + "-Text");
-            }
         }
     }
 }
