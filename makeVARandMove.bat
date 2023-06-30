6fee08540fb22b23a1c189db31d63271d18c9099
Mike Tremp <mike.tremp@loanboox.ch>
2023-06-30 18:20:37 +0200

    updated meta info

--- makeVARandMove.bat
+++ makeVARandMove.bat
@@ -1,3 +1,3 @@
-copy .\meta.zip "D:\Games\Epic Games\Final Fantasy XV\AddonPackages\Gardan\Gardan.Positioner.4.var"
+copy .\meta.zip "D:\Games\Epic Games\Final Fantasy XV\AddonPackages\Gardan\Gardan.Positioner.5.var"
 del .\meta.zip
 pause
