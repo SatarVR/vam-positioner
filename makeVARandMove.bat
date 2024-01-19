eec8a951e84ad71169b63ea7491f8b01a35ff338
Mike Tremp <mike.tremp@loanboox.ch>
2024-01-19 16:31:18 +0100

    updated batch file

--- makeVARandMove.bat
+++ makeVARandMove.bat
@@ -1,3 +1,3 @@
-copy .\meta.zip "D:\Games\Epic Games\Final Fantasy XV\AddonPackages\Gardan\Gardan.Positioner.8.var"
+copy .\meta.zip "D:\Games\Epic Games\Final Fantasy XV\AddonPackages\Gardan\Gardan.Positioner.9.var"
 del .\meta.zip
 pause
